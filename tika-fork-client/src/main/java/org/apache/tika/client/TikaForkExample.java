6b4ae52b86ad135842cca6d9b51a6d6a486c7460
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2020-11-01 10:27:24 -0600

    specify the jvm args and stuff to demonstration that you can do so.

--- tika-fork-client/src/main/java/org/apache/tika/client/TikaForkExample.java
+++ tika-fork-client/src/main/java/org/apache/tika/client/TikaForkExample.java
@@ -8,6 +8,7 @@ import org.apache.tika.parser.ParseContext;
 import java.io.File;
 import java.io.FileInputStream;
 import java.nio.file.Paths;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -23,6 +24,13 @@ public class TikaForkExample {
     parserArgs.put("maxPackageEntryDepth", String.valueOf(20));
 
     ForkParser forkParser = new ForkParser(Paths.get(pathToTikaMainDist), new ParserFactoryFactory("org.apache.tika.fork.main.CollectingParserFactory", parserArgs));
+
+    forkParser.setMaxFilesProcessedPerServer(1000);
+    forkParser.setPoolSize(5);
+    forkParser.setJavaCommand(Arrays.asList("java", "-Xmx1g", "-Djava.awt.headless=true"));
+    forkParser.setServerWaitTimeoutMillis(60000);
+    forkParser.setServerParseTimeoutMillis(60000);
+    forkParser.setServerPulseMillis(1000);
 
     CollectingParser collectingParser = new CollectingParser(forkParser, fileToParse.getName());
 
